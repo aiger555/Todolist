0ce31b4019332361f10aa700802a23dfefcd275d
Aigerim Nuralieva <aigerim.nuralieva@iaau.edu.kg>
2021-08-12 20:12:20 +0600

    Set up CRUD for task

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import  get_object_or_404
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -29,4 +30,28 @@ class TaskAPIView(APIView):
 
 
 
-# class TaskDetailView(APIView):
+class TaskDetailAPIView(APIView):
+    
+    def get(self, request, id):
+        task = get_object_or_404(Task, id=id)
+        serializer = TaskSerializer(instance=task)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        task = get_object_or_404(Task, id=id)
+        serializer= TaskSerializer(instance = task, data = request.data)
+
+        if serializer.is_valid():
+            body = serializer.validated_data.get('body')
+            est = serializer.validated_data.get('estimated_finish_time')
+            task.body = body
+            task.estimated_finish_time = est
+            task.save()
+            return Response(serializer.data)
+        return Response({'detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        task = get_object_or_404(Task, id=id)
+        task.delete()
+        return Response({'detail':'success'}, status=status.HTTP_204_NO_CONTENT)
+        
