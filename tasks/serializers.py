295a5061cb04b480528ca015a6cc04d6b7080da7
Aigerim Nuralieva <aigerim.nuralieva@iaau.edu.kg>
2021-08-14 23:47:39 +0600

    Set up task_is_finished

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -9,12 +9,16 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-
         fields = ['id', 'username', 'email']
+
+
 class TaskSerializer(serializers.Serializer):
+
     creator = UserSerializer(read_only=True)
     body = serializers.CharField()
     estimated_finish_time = serializers.DateTimeField(format='%d-%m-%Y %H:%M', 
                                                       input_formats=['%d-%m-%Y %H:%M', ])
     created_datetime = serializers.DateTimeField(read_only=True, format="%d-%m-%Y %H:%M")
     is_completed = serializers.BooleanField(read_only=True)
+
+
