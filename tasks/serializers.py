e08115ce9cfe95e3ba88853c430b6f38b9c35245
Aigerim Nuralieva <aigerim.nuralieva@iaau.edu.kg>
2021-08-15 00:10:51 +0600

    Change to ModelSerializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,6 +1,8 @@
+from django.db.models.fields import DateTimeField
+from tasks.models import Task
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-
+from tasks.models import Task
 
 User = get_user_model()
 
@@ -12,13 +14,28 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email']
 
 
-class TaskSerializer(serializers.Serializer):
+class TaskSerializer(serializers.ModelSerializer):
 
     creator = UserSerializer(read_only=True)
-    body = serializers.CharField()
     estimated_finish_time = serializers.DateTimeField(format='%d-%m-%Y %H:%M', 
                                                       input_formats=['%d-%m-%Y %H:%M', ])
-    created_datetime = serializers.DateTimeField(read_only=True, format="%d-%m-%Y %H:%M")
-    is_completed = serializers.BooleanField(read_only=True)
+    created_datetime = serializers.DateTimeField(format='%d-%m-%Y %H:%M')
+    
+    class Meta:
+        model = Task
+        fields = ['id', 'creator', 'body', 'estimated_finish_time', 'created_datetime', 
+                'is_completed']
+        read_only_fields = ['created_datetime', 'is_completed', ]
+
+
+
+
+
+    # creator = UserSerializer(read_only=True)
+    # body = serializers.CharField()
+    # estimated_finish_time = serializers.DateTimeField(format='%d-%m-%Y %H:%M', 
+    #                                                   input_formats=['%d-%m-%Y %H:%M', ])
+    # created_datetime = serializers.DateTimeField(read_only=True, format="%d-%m-%Y %H:%M")
+    # is_completed = serializers.BooleanField(read_only=True)
 
 
